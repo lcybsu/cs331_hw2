a4711547286e557166a6aaf733d7b8e072bf36d8
Chenyi Ling <38633285+lcybsu@users.noreply.github.com>
2020-02-10 17:56:06 -0900

    Add files via upload
    3rd function

--- pa2.lua
+++ pa2.lua
@@ -22,32 +22,102 @@ end
 
 --   
 
-
 function pa2.concatMax(str,n)
 	local len = string.len(str)
 	local reps = math.floor(n/len)
 	local newstr = string.rep(str,reps)
 	return newstr
-
 end
 
 
 
-function pa2.collatz(k)
-	
-	while k ~= nil do
+function pa2.collatz(k)	
+	coroutine.yield(k)		
+	while k ~= 1 do	
 		if k % 2 == 0 then
 			k = k/2	
 		else
 			k = 3*k + 1
-		end	
-		
-		print("yield".." "..k)
-		coroutine.yield(k)
-		
-		
-	end
-	
+		end			
+		coroutine.yield(k)	
+	end	
 end
 
+
+function pa2.allSubs(s)
+	
+	local len = string.len(s)
+	print("string length is "..len)
+	
+	
+	
+	--return function()
+		local arr_sub = {""}
+		local substr
+		local i,j
+		local index = 0
+		if len == 0 then
+			table.insert(arr_sub,"")
+			print("first element is "..arr_sub[1])
+			return
+		end
+		
+		--return function()
+		
+		while index ~= len*len+1 do 
+			
+			return function()
+				for i = 1,len do
+		
+					for j = 1,len do 
+			
+						substr=string.sub(s,j,i)
+						
+						print("substr is "..substr)
+						table.insert(arr_sub,substr)
+						index = i*j
+						j = j+1
+						
+						return arr_sub[index]
+						
+					end
+			
+					i = i+1
+				end
+			
+			end
+			
+			
+			
+		end
+	
+		print("first thing is  "..arr_sub[1])
+		print("second thing is  "..arr_sub[2])
+		--return arr_sub
+	
+	
+		--end
+
+		
+end
+
+
+
+
+
+
 return pa2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
